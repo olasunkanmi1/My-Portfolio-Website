11eaa7c502b3d195a9e2b6b56917de917f5582cd
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-11-11 16:15:18 -0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import GlobalStyles from './GlobalStyles';
+
+//components
+import { Navbar,  }from './components'
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
