6131a08f9038b9a3dfd64ba318845a572a167254
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-11-11 18:06:22 -0100

    commit

---
+++ src/components/Navbar/Styled.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const NavbarStyled = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 85px;
+    width: 100%;
+    z-index: 10;
+    position: fixed;
+    top: 0;
+    padding: 0rem 10rem;
+
+    @media screen and (max-width: 1200px) {
+        padding: 1rem 8rem;
+    }
+    @media screen and (max-width: 1000px) {
+        padding: 1rem 7rem;
+    }
+
+    @media screen and (max-width: 920px) {
+        padding: 1rem 6rem;
+    }
+    @media screen and (max-width: 768px) {
+        padding: 1rem 5rem;
+    }
+    @media screen and (max-width: 600px) {
+        padding: 1rem 4rem;
+    }
+    @media screen and (max-width: 500px) {
+        padding: 1rem 3rem;
+    }
+
+    @media screen and (max-width: 450px) {
+        padding: 1rem 2rem;
+    }
+
+    .logo {
+        height: 100%;
+
+        img {
+            height: 100%;
+            width: 100px;
+        }
+    }
+
+    p {
+        
+        color: var(--secondary);
+        font-size: 40px;
+        font-weight: 500;
+    }
+`
+
+export default NavbarStyled;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import NavbarStyled from "./Styled";
+import logo from "../../assets/logo.png";
+import ScrollSpy from "react-scrollspy";
 
 const index = () => {
-  return <div>yuyhi</div>;
+  return (
+    <NavbarStyled>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+
+      <Scrollspy
+        className="scrollspy"
+        items={["home", "about", "skills", "portfolio", "contact"]}
+        currentClassName="current"
+      >
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>SKILLS</li>
+      </Scrollspy>
+    </NavbarStyled>
+  );
 };
 
 export default index;
