1210ab0ff335b0cdd81c8dc74d6ae564776cfab1
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-12-05 23:55:58 -0100

    Update skills section

--- src/data/SkillsData.jsx
+++ src/data/SkillsData.jsx
@@ -2,6 +2,7 @@ import { ReactComponent as Html } from ".././assets/icons/html.svg";
 import { ReactComponent as Css } from ".././assets/icons/css.svg";
 import { ReactComponent as Js } from ".././assets/icons/js.svg";
 import { ReactComponent as React } from ".././assets/icons/react.svg";
+import { ReactComponent as Next } from ".././assets/icons/next.svg";
 import { ReactComponent as Bootstrap } from ".././assets/icons/bootstrap.svg";
 import { ReactComponent as Scss } from ".././assets/icons/scss.svg";
 import { ReactComponent as Mui } from ".././assets/icons/mui.svg";
@@ -11,7 +12,8 @@ export const SkillsData = [
   { svg: <Html />, name: "HTML5" },
   { svg: <Css />, name: "CSS3" },
   { svg: <Js />, name: "JavaScript" },
-  { svg: <React />, name: "React Js" },
+  { svg: <React />, name: "React JS" },
+  { svg: <Next />, name: "Next JS" },
   { svg: <Bootstrap />, name: "Bootstrap" },
   { svg: <Scss />, name: "SCSS" },
   { svg: <Mui />, name: "Material UI" },
